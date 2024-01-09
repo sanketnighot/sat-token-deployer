33d69f0c087d4a1eea1b63f4d474071962e84ec1
sanketnighot <sanketnighot25@gmail.com>
2024-01-09 13:05:52 +0530

    Added Wallet Connection

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,30 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import Link from 'next/link';
+import { dappClient } from '../utils/walletconnect'
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [account, setAccount] = useState(false);
+    useEffect(() => {
+        (async () => {
+            // TODO 5.b - Get the active account
+            const accounts = await dappClient().getAccount();
+            setAccount(accounts.account);
+
+        })();
+    }, []);
+
+    const onConnectWallet = async () => {
+        await dappClient().connectAccount();
+        const accounts = await dappClient().getAccount();
+        setAccount(accounts.account);
+    };
+
+    const onDisconnectWallet = async () => {
+        await dappClient().disconnectWallet();
+        setAccount(false);
+    };
 
     return (
         <nav className="drop-shadow-xl z-40 fixed w-full bg-black">
@@ -26,9 +47,17 @@ const Navbar = () => {
                         </div>
                     </div>
                     {/* Connect Wallet Button */}
-                    <div className="hidden md:flex space-x-1">
-                        <button className="px-4 py-2 bg-black-500 text-white rounded hover:bg-black-600 transition duration-300">
-                            Connect Wallet
+                    <div className="hidden md:flex space-x-2">
+                        <button className="px-4 py-2 bg-black-500 text-white rounded hover:bg-black-600 transition duration-300" onClick={!account ? onConnectWallet : onDisconnectWallet}>
+                            {!account ? <div className="flex">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
+                                </svg> &nbsp; Connect Wallet
+                            </div> : <div className="flex">
+                                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3" />
+                                </svg> &nbsp; Disconnect Wallet
+                            </div>}
                         </button>
                     </div>
 
@@ -56,8 +85,8 @@ const Navbar = () => {
                 <Link href="/" className="block py-2 px-4 text-sm hover:bg-black-500 transition duration-300 text-white">Home</Link>
                 <Link href="/collection" className="block py-2 px-4 text-sm hover:bg-black-500 transition duration-300 text-white">Collection</Link>
                 <div className="flex">
-                    <button className="block px-4 py-2 bg-black-500 text-white rounded hover:bg-black-500 transition duration-300">
-                        Connect Wallet
+                    <button className="block px-4 py-2 bg-black-500 text-white rounded hover:bg-black-500 transition duration-300" onClick={!account ? onConnectWallet : onDisconnectWallet}>
+                        {!account ? "Sync" : "Disconnect"}
                     </button>
                 </div>
             </div>
