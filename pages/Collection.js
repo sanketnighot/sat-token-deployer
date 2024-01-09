ff7758e0ab25d213a92c4ec77d43217df76f15e0
sanketnighot <sanketnighot25@gmail.com>
2024-01-10 01:55:05 +0530

    UI updates

--- pages/Collection.js
+++ pages/Collection.js
@@ -67,7 +67,7 @@ const Collection = () => {
                     <button
                         type="submit"
                         className="p-2 text-white bg-black rounded flex text-center justify-center">
-                        {"Next"} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        {"Next"} &nbsp; <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
                         </svg>
 
